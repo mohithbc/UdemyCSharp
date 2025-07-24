c36c6e94f4da6f1c69c57b92601db01bd82e6b3e
mohithbc <mohithbc02@gmail.com>
2025-07-24 17:17:34 +0530

    added operations and evaluation

---
+++ OperatorsAndOrderOfEvaluation/OperatorsAndOrderOfEvaluation/Program.cs
@@ -0,0 +1,14 @@
+﻿// Operators and Order of Evaluation
+int num1 = 5;
+int num2 = 13;
+
+// Concatination
+Console.WriteLine("Age is" + num1);
+Console.WriteLine("Addition numl + num2 = " + num1 + num2);
+
+//Order of Evaluation
+Console.WriteLine("Subtraction num1 - num2 -" + num1 - num2); // solved this below
+Console.WriteLine("Subtraction num1 - num2 " + (num1 - num2));
+
+Console.WriteLine("Subtraction num1 - num2 = 5" - num2);
+// "Subtraction numi - num2 = 5" - 13
