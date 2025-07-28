c66e3c53d9c135efa94b2bfe35ecdaf0c1f814fb
mohithbc <mohithbc02@gmail.com>
2025-07-28 22:41:47 +0530

    added till landing a roctket

---
+++ LandingARocket/LandingARocket/Program.cs
@@ -0,0 +1,14 @@
+﻿string rocket = "     |\r\n     |\r\n    / \\\r\n   / _ \\\r\n  |.o '.|\r\n  |'._.'|\r\n  |     |\r\n ,'|  | |`.\r\n/  |  | |  \\\r\n|,-'--|--'-.|";
+
+for (int counter = 10; counter >= 0; counter--)
+{
+    Console.Clear();
+    Console.WriteLine("Counter is " + counter);
+    Console.WriteLine(rocket);
+    rocket = "\r\n" + rocket;
+    Thread.Sleep(1000);
+}
+Console.WriteLine("The Rocket has Landed!");
+
+Console.ReadKey();
+
---
+++ LoopBasics/LoopBasics/Program.cs
@@ -0,0 +1,42 @@
+﻿
+// In string \ is an escape character
+// \n stands for new line
+
+string myString = "Hi \nHi";
+
+// for loop
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine("I is " + i);
+    Thread.Sleep(1000);
+}
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine("I is " + i);
+}
+
+// While loop
+
+int counter = 0;
+while (counter < 10)
+{
+    Console.WriteLine(counter);
+    counter++;
+}
+
+// while loop where it really shines
+
+Console.WriteLine("Enter go or stay");
+string userChoice = Console.ReadLine();
+
+while (userChoice == "go")
+{
+    Console.WriteLine("Go for a mile");
+    Console.WriteLine("Wanna keep going enter go. Else type no or something");
+    userChoice = Console.ReadLine();
+}
+Console.WriteLine("Finally you are staying");
+
+Console.ReadKey();
