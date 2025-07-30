72463f4c22bf56a5f63b915b2ecad9c59dc84cde
mohithbc <mohithbc02@gmail.com>
2025-07-30 22:12:54 +0530

    added till voaid method with parameter

---
+++ MethodsApp/MethodsApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// Methods are structured like this
+// Modifier returnType MethodName(Parameters){
+// code block
+// }
+
+// definition of the void method
+void myFirstMethod()
+{
+    Console.WriteLine("MyFirtMethod was called");
+}
+
+// calling of the method
+myFirstMethod();
+Console.WriteLine("This is outside of the method");
+Console.ReadKey();
---
+++ VoidMethodWithParameter/VoidMethodWithParameter/Program.cs
@@ -0,0 +1,18 @@
+﻿void writeSomething()
+{
+    Console.WriteLine("Im writing something");
+}
+
+writeSomething();
+
+// a method that has the parameter myString of type String
+// method declaration
+void writeSometingSpecific(string myString)
+{
+    Console.WriteLine("You passed this argument to me " + myString);
+}
+
+string myUSerName = "mohith";
+// calling method and using an argument calle dmyusername
+writeSometingSpecific("Stuppid");// can pass myUsername here also if we want
+Console.WriteLine("This is ouside of the method");
