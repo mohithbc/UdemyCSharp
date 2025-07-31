475d88f622976ace6ad4d049a06ac61fec911067
mohithbc <mohithbc02@gmail.com>
2025-07-31 15:48:51 +0530

    added till oldStyle

---
+++ VoidMethodWithParameterTwo/VoidMethodWithParameterTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿int addTwoValues(int value1, int value2)
+{
+    int result = value1 + value2;
+    return result;
+}
+Console.WriteLine("enter first and second value to sum");
+Console.Write("Enter first number: ");
+int Uvalue1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int Uvalue2 = int.Parse(Console.ReadLine());
+
+int myResult = addTwoValues(Uvalue1, Uvalue2);
+Console.WriteLine("Result is : " + myResult);
+
---
+++ oldStyle/oldStyle/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace oldStyle
+{
+    internal class Program
+    {
+        // int myResult;
+        // now myresult is a field/instance variable you can call it a 
+        // global variable, it is globaly available atleast inside of our class
+        static void Main(string[] args)
+        {
+            int myResult;
+
+            Console.WriteLine("Enter a number, I'll add 10 to it!");
+            int num1 = int.Parse(Console.ReadLine());
+
+            myResult = AddTwoValues(num1, 10);
+            Console.WriteLine("The result is " + myResult);
+
+            Console.ReadKey();
+        }
+
+        static int AddTwoValues(int value1, int value2)
+        {
+            int result = value1 + value2;
+            return result;
+        }
+
+        static int SubtractTwoValues(int value1, int value2)
+        {
+            int result = value1 - value2;
+            return result;
+        }
+    }
+}
