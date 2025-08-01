a6ac9e373a0cfa75147db443c15803219593aff2
mohithbc <mohithbc02@gmail.com>
2025-08-01 17:49:31 +0530

    Added weather simulator

---
+++ WeatherSimulator/WeatherSimulator/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace WeatherSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number of days to stimulate : ");
+            int days = int.Parse(Console.ReadLine());
+            int[] temperature = new int[days];
+            string[] conditions = { "Sunny", "Rainy", "Cloudy", "Snowy" };
+            string[] weatherConditions = new string[days];
+
+            Random random = new Random();
+
+            for (int i = 0; i < days; i++)
+            {
+                temperature[i] = random.Next(-10, 40);
+                weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            }
+
+            // double averageTemp = calculateAverage(temperature);
+            Console.WriteLine($"Average Temperature is : {calculateAverage(temperature)}");
+            Console.WriteLine($"The max temp was : {temperature.Max()}");
+            Console.WriteLine($"The min temp was : {temperature.Min()}");
+            Console.ReadKey();
+        }
+
+        static double calculateAverage(int[] temperature)
+        {
+            double sum = 0;
+            for (int i = 0; i < temperature.Length; i++)
+            {
+                sum += temperature[i];
+            }
+
+            // can use foreach also
+            //foreach (int i in temperature)
+            //{
+            //    sum += i;
+            //}
+
+            double average = sum / temperature.Length;
+
+            return average;
+        }
+
+        // the below piece of code is what a .min() method does
+
+        //static int minTemperature(int[] temperature)
+        //{
+        //    int min = temperature[0];
+        //    foreach (int temp in temperature)
+        //    {
+        //        if (temp < min)
+        //        {
+        //            min = temp;
+        //        }
+        //    }
+        //    return min;
+        //}
+    }
+}
