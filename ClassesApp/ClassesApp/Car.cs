19a9f5ead56035fabe041914f3c82c7cb1173b9c
mohithbc <mohithbc02@gmail.com>
2025-08-06 23:31:23 +0530

    Creating first own class

---
+++ ClassesApp/ClassesApp/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesApp
+{
+    /* Its internal which means it can be 
+     accessed only within the same class*/
+    internal class Car
+    {
+        // Constructor - is very similar to a method
+        // but its diffrent like it has the exact same name
+        // as class itself and it doesnot have a return type
+        // constructor is called or executed whenever
+        // a new object of car is created.
+        public Car()
+        {
+            Console.WriteLine("An object of car has been created");
+        }
+    }
+}
---
+++ ClassesApp/ClassesApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ClassesApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating an object of the class car
+            // Or creating an instance of the class Car
+            Car audi = new Car();
+            // Creating an another object
+            Car bmw = new Car();
+
+            Console.ReadKey();
+
+        }
+    }
+}
