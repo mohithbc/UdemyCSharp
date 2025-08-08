c5f83911718d74c3ecdb05e7a0b13e22186cc900
mohithbc <mohithbc02@gmail.com>
2025-08-08 17:55:41 +0530

    Member variables & custom constructor added

--- ClassesApp/ClassesApp/Car.cs
+++ ClassesApp/ClassesApp/Car.cs
@@ -10,14 +10,23 @@ namespace ClassesApp
      accessed only within the same class*/
     internal class Car
     {
-        // Constructor - is very similar to a method
-        // but its diffrent like it has the exact same name
-        // as class itself and it doesnot have a return type
-        // constructor is called or executed whenever
-        // a new object of car is created.
-        public Car()
+        /*Member variable - its a field inside of a class, so its a varibale inside of a class and outside of the methods*/
+        // if you are using a filed inside of the class that you are creating then it is a member variable of that class, model now is the 
+        // member variable of the car class
+
+        // private hides the variable from other classes means u cannot access it in program.cs as bmw._model
+        private string _model = "";
+        private string _brand = "";
+
+        // Constructor - is very similar to a method but its diffrent, like it has the exact same name
+        // as class itself and it doesnot have a return type constructor is called or executed whenever a new object of car is created.
+
+        // Custom Constructor
+        public Car(string model, string brand)
         {
-            Console.WriteLine("An object of car has been created");
+            _model = model;
+            _brand = brand;
+            Console.WriteLine($"A car of brand {_brand} & model {_model} has been created");
         }
     }
 }
--- ClassesApp/ClassesApp/Program.cs
+++ ClassesApp/ClassesApp/Program.cs
@@ -6,9 +6,9 @@
         {
             // Creating an object of the class car
             // Or creating an instance of the class Car
-            Car audi = new Car();
+            Car audi = new Car("A3","audi");
             // Creating an another object
-            Car bmw = new Car();
+            Car bmw = new Car("I7", "BMW");
 
             Console.ReadKey();
 
