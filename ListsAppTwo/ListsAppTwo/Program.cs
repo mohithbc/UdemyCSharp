6c079e3f8e52ce4df7b0916d7a073dde6da4d6f4
mohithbc <mohithbc02@gmail.com>
2025-09-01 02:24:59 -0400

    Added till ListsAppTwo

---
+++ ListsAppTwo/ListsAppTwo/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ListsAppTwo
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> products = new List<Product>
+            {
+                new Product { Name = "Laptop", Price = 999.99 },
+                new Product { Name = "Smartphone", Price = 699.99 },
+                new Product { Name = "Tablet", Price = 399.99 }
+            };
+
+            List<Product> CheapProducts = products.Where(p => p.Price < 700).ToList();
+
+            Console.WriteLine("Available Products: ");
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"- {product.Name}: ${product.Price}");
+            }
+
+            Console.WriteLine("\nAffordable Products (under $700): ");
+            foreach (var product in CheapProducts)
+            {
+                Console.WriteLine($"- {product.Name}: ${product.Price}");
+            }
+        }
+    }
+}
