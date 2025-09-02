37718337d027ebf3c5afbd2891b6ae3bdce7e411
mohithbc <mohithbc02@gmail.com>
2025-09-02 05:05:49 -0400

    Added till nullables

---
+++ Nullables/Nullables/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Nullables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? age = null; // This line will cause a compile-time error
+            int myAge = 30;
+            if (age.HasValue)
+            {
+                Console.WriteLine($"Age is {age.Value}");
+                int sum = age.Value + myAge;
+            }
+            else
+            {
+                Console.WriteLine("Age is not provided.");
+            }
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
