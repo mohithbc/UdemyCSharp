766487be1838334c34dd053e9a33157bd7f7f2cf
mohithbc <mohithbc02@gmail.com>
2025-09-02 08:28:00 -0400

    Added till dictionaries1-2

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Dictionaries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Key - Value
+            // Declaring and initializing a dictionary
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+            employees.Add(1, "John");
+            employees.Add(2, "Jane");
+            employees.Add(3, "Jack");
+            employees.Add(4, "Jill");
+
+            // Accesing items in a dictionary
+            string name = employees[2];
+            Console.WriteLine(name); // Output: Jane
+
+            employees[2] = "Jane Smith"; // Updating an item
+            employees.Remove(4); // Removing an item
+
+            // Iterating through a dictionary
+            foreach (KeyValuePair<int, string> kvp in employees)
+            {
+                Console.WriteLine($"ID: {kvp.Key}, Name: {kvp.Value}");
+            }
+
+        }
+    }
+}
