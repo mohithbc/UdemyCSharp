af2ae2d52199bb4d46fd12d0c75c7bcf8b365877
mohithbc <mohithbc02@gmail.com>
2025-09-03 08:29:25 -0400

    Added till DictionariesTwo

---
+++ DictionariesTwo/DictionariesTwo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace DictionariesTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           var codes = new Dictionary<string, string>
+           {
+               ["US"] = "United States",
+               ["FR"] = "France",
+               ["ES"] = "Spain"
+           };
+
+            if(codes.TryGetValue("FR", out var country))
+            {
+                Console.WriteLine(country);
+            }
+            foreach(var item in codes)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
