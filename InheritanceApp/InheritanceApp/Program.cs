a8a0d0c70a2559e5f02e07df0e88281f74b9ab46
mohithbc <mohithbc02@gmail.com>
2025-09-08 08:12:30 -0400

    Added till InheritanceApp

--- InheritanceApp/InheritanceApp/Program.cs
+++ InheritanceApp/InheritanceApp/Program.cs
@@ -6,7 +6,10 @@
         {
             Dog myDog = new Dog();
             myDog.Eat();  // Inherited method from Animal class
-            myDog.Bark(); // Method from Dog class
+            myDog.MakeSound(); // Overridden method from Dog class
+
+            Cat myCat = new Cat();
+            myCat.MakeSound(); // Overridden method from Cat class
         }
     }
     // Base Class (Parent class or Superclass):
@@ -17,13 +20,19 @@
         {
             Console.WriteLine("Eatting");
         }
+        // Virtual method to allow overriding in derived classes
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
     }
     // Derived Class (Child class or Subclass):
     // The class that inherits members from the base class
     class Dog : Animal
     {
-        public void Bark()
+        public override void MakeSound() // Overriding the MakeSound method
         {
+            base.MakeSound(); // Calling the base class method
             Console.WriteLine("Barking");
         }
     }
@@ -33,7 +42,7 @@
     // Thus, both Dog and Cat have access to the Eat method of Animal
     class Cat : Animal
     {
-        public void Meow()
+        public override void MakeSound()
         {
             Console.WriteLine("Meowing");
         }
