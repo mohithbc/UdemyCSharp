9aa02b273f860cd81575b8967f5eca7e967bf01f
mohithbc <mohithbc02@gmail.com>
2025-09-15 22:26:50 +0530

    Added till InterfacesApp

---
+++ InterfacesApp/InterfacesApp/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace InterfacesApp
+{
+    public interface IAnimal
+    {
+        // Interface method (does not have a body)
+        void MakeSound();
+        void Eat(string food);
+    }
+    // Dog "implements" the IAnimal interface
+    public class Dog : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Dog ate " + food);
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+    public class Cat : IAnimal
+    {
+        public void Eat(string food)
+        {
+            Console.WriteLine("Cat ate " + food);
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog rocky = new Dog();
+            rocky.MakeSound();
+            rocky.Eat("bone");
+            Cat tom = new Cat();
+            tom.MakeSound();
+            tom.Eat("fish");
+        }
+    }
+}
