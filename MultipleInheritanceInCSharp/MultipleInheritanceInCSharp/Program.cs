2e545ba45a4cc8458ffc76111441e37b2b64263f
mohithbc <mohithbc02@gmail.com>
2025-09-23 22:28:01 +0530

    Added till multiple inheratance in CSharp

---
+++ MultipleInheritanceInCSharp/MultipleInheritanceInCSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace MultipleInheritanceInCSharp
+{
+    public interface IPrintable
+    {
+        void Print();
+    }
+    public interface IScannable
+    {
+        void Scan();
+    }
+    public class MultiFunctionPrinter : IPrintable, IScannable
+    {
+        public void Print()
+        {
+            Console.WriteLine("Printing document...");
+        }
+        public void Scan()
+        {
+            Console.WriteLine("Scanning document...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MultiFunctionPrinter mfp = new MultiFunctionPrinter();
+            mfp.Print();
+            mfp.Scan();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
