5f5fb4fe39543fc434a75befd833e0d0a2dc36f1
mohithbc <mohithbc02@gmail.com>
2025-10-13 17:37:33 +0530

    Added till EnumsC

---
+++ EnumsC/EnumsC/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EnumsC
+{
+    enum Day {  Mo, Tu, We, Th, Fr, Sa, Su };
+    enum Month {  Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day fr = Day.Fr;
+            Day su = Day.Su;
+
+            Day a = Day.Fr;
+
+
+            Console.WriteLine(fr==a);
+            Console.WriteLine((int)Day.Mo);
+            Console.WriteLine((int)Month.Jan);
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
