322b9a0563c87d1342927e65520091658d63afba
mohithbc <mohithbc02@gmail.com>
2025-10-14 09:28:45 +0530

    Added till EnumnsC

--- EnumsC/EnumsC/Program.cs
+++ EnumsC/EnumsC/Program.cs
@@ -1,12 +1,13 @@
 ﻿namespace EnumsC
 {
+    // Define an enumeration for the days of the week
     enum Day {  Mo, Tu, We, Th, Fr, Sa, Su };
     enum Month {  Jan = 1, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec };
     class Program
     {
         static void Main(string[] args)
         {
-            Day fr = Day.Fr;
+            Day fr = Day.Fr;//fr is a variable of type Day and is assigned the value Day.Fr
             Day su = Day.Su;
 
             Day a = Day.Fr;
