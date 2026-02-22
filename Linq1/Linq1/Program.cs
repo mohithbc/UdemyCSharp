a240a53e565354849f5fcfdec6600eb52a735622
mohithbc <mohithbc02@gmail.com>
2026-02-22 11:19:36 +0530

    added linq demo

---
+++ Linq1/Linq1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Linq1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            OddNumbers(numbers);
+            Console.ReadKey();
+        }
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd numbers:");
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 != 0 select number;
+            Console.WriteLine(oddNumbers);
+            foreach(int i in oddNumbers)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
