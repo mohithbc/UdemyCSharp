305776182a8ad55c1cdf3111f6fdfef7ceb367a4
mohithbc <mohithbc02@gmail.com>
2026-02-24 20:47:48 +0530

    Added till multi generic

---
+++ MultipleGenericTypes/MultipleGenericTypes/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultipleGenericTypes
+{
+    internal class Box<TFirst, TSecond>
+    {
+        public TFirst First { get; set; }
+        public TSecond Second { get; set; }
+        public Box(TFirst first, TSecond second) 
+        {
+            First = first; 
+            Second = second; 
+        }
+        public void Display()
+        {
+            Console.WriteLine($"First: {First} Second: {Second}");
+        }
+    }
+}
---
+++ MultipleGenericTypes/MultipleGenericTypes/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace MultipleGenericTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int, string> box = new Box<int, string>(100, "one hundred");
+            box.Display();
+        }
+    }
+}
