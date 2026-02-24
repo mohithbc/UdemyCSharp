1b5efbba361a59361afcfc39a32fc6927f4396fd
mohithbc <mohithbc02@gmail.com>
2026-02-24 21:03:12 +0530

    Added GenericMethodsWithoutGenericClasses

---
+++ GenericMethodsWithoutGenericClasses/GenericMethodsWithoutGenericClasses/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace GenericMethodsWithoutGenericClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Logger logger = new Logger();
+            logger.Log<int>(10);
+            logger.Log<string>("hello world");
+            //no need to specify int or string as its geenric we can do
+            logger.Log(new {name = "mohith", age  = 21});
+        }
+    }
+}
