4dd919a5c48627ce34f343d140850b2a64a3e254
mohithbc <mohithbc02@gmail.com>
2026-02-24 20:36:16 +0530

    Added MyGenericClassTypeInMethod

---
+++ MyGenericClassTypeInMethod/MyGenericClassTypeInMethod/Box.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericClassTypeInMethod
+{
+    internal class Box<T>
+    {
+        private T content;
+        public Box(T initialValue)
+        {
+            content = initialValue;
+        }
+
+        public void UpdateContent(T newContent)
+        {
+            content = newContent;
+            Console.WriteLine($"Updated content to {content}");
+        }
+        public T GetContent()
+        {
+            return content;
+        }
+    }
+}
---
+++ MyGenericClassTypeInMethod/MyGenericClassTypeInMethod/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace MyGenericClassTypeInMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> boxStr = new Box<string>("hello world");
+            boxStr.UpdateContent("Teach c#");
+            Console.WriteLine(boxStr.GetContent());
+        }
+    }
+}
