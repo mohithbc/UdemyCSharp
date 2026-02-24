6b1707b670c603be3fd42742ed25409129c1b70b
mohithbc <mohithbc02@gmail.com>
2026-02-24 21:35:44 +0530

    Custom interface constraint

---
+++ CustomInterfaceConstraint/CustomInterfaceConstraint/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace CustomInterfaceConstraint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Repository<Product> repository = new Repository<Product>();
+            var Product = new Product();
+            repository.Add(Product);
+        }
+    }
+    class Product : IEntity
+    {
+        public int Id { get; }
+    }
+}
---
+++ CustomInterfaceConstraint/CustomInterfaceConstraint/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterfaceConstraint
+{
+    internal interface IEntity
+    {
+        int Id { get; }
+    }
+    internal class Repository<T> where T : IEntity
+    {
+        private List<T> values = new List<T>();
+        public void Add(T entity)
+        {
+
+        }
+    }
+}
