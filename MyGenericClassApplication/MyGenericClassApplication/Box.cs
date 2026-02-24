f1ee2622120236c87d5bcfec33b1f435c977e6da
mohithbc <mohithbc02@gmail.com>
2026-02-24 20:09:42 +0530

    added MyGenericClassApplication

---
+++ MyGenericClassApplication/MyGenericClassApplication/Box.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGenericClassApplication
+{
+    internal class Box<T>
+    {
+        public T Content { get; set; }
+        public string Log()
+        {
+            return $"Box contains {Content}";
+        }
+    }
+}
---
+++ MyGenericClassApplication/MyGenericClassApplication/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MyGenericClassApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+            box.Content = 1;
+            Console.WriteLine(box.Log());
+
+            Box<string> boxStr = new Box<string>();
+            boxStr.Content = "hello from generic class";
+            Console.WriteLine(boxStr.Log());
+        }
+    }
+}
