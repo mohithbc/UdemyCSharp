4321ea95275906e7a3c1151b1a34a8cd3206faa4
mohithbc <mohithbc02@gmail.com>
2026-02-24 21:19:27 +0530

    added constraints for generic classes

---
+++ ConstraintsForGenericClasses/ConstraintsForGenericClasses/Box.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstraintsForGenericClasses
+{
+    // Constraints 
+    internal class Box<T> where T : class 
+    {
+    }
+}
---
+++ ConstraintsForGenericClasses/ConstraintsForGenericClasses/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConstraintsForGenericClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<Book> bookBox = new Box<Book>();
+            Box<int> boxInt = new Box<int>();
+        }
+    }
+    class Book
+    {
+
+    }
+}
