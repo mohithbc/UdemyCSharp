47de7962395b11b9950b35da8050e758daaf4821
mohithbc <mohithbc02@gmail.com>
2026-02-25 22:01:48 +0530

    generic delegate

---
+++ GenericDelegate/GenericDelegate/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace GenericDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action action = () => 
+            {
+                Console.WriteLine("Hello World"); 
+            };
+            action();
+
+            Action<int> numPrint = (x) =>
+            {
+                Console.WriteLine(x);
+            };
+            numPrint(20);
+
+            Action<float, int, double> sum = (a, b, c) =>
+            {
+                Console.WriteLine(a+b+c);
+            };
+            sum(10, 20, 30);
+        }
+    }
+}
