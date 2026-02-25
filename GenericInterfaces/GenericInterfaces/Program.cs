8d3904db55d4c34b7f65040130acddd013260567
mohithbc <mohithbc02@gmail.com>
2026-02-25 07:42:56 +0530

    Generic interfaces

---
+++ GenericInterfaces/GenericInterfaces/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace GenericInterfaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    internal interface IEntity
+    {
+        int Id { get; }
+    }
+    internal interface IRepository<T> where T : class
+    {
+        void Add(T entity);
+        void Remove(T entity);
+    }
+    internal class Product : IEntity 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    internal class ProductRepository : IRepository<Product>
+    {
+        public void Add(Product entity)
+        {
+
+        }
+        public void Remove(Product entity)
+        {
+
+        }
+    }
+}
