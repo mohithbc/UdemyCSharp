621166c2052b77cf99725088e2cea3d0c99f36a3
mohithbc <mohithbc02@gmail.com>
2026-02-25 08:03:02 +0530

    Generic reflections

---
+++ GenericReflections/GenericReflections/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+namespace GenericReflections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type type = typeof(ConfigurationManager<>);
+
+            string myName = "mohith";
+            if(myName.GetType() == typeof(string))
+            {
+                Console.WriteLine("Its a string");
+            }
+        }
+    }
+
+    internal class ConfigurationManager<T>
+    {
+        public T LoadedConfiguration { get; private set; }
+        public ConfigurationManager(T config)
+        {
+            LoadedConfiguration = config;
+        }
+        public static void SaveConfiguration(T configToSave)
+        {
+
+        }
+    }
+}
