2155684d94ab1e5639980a7bd682cc399772f0b4
mohithbc <mohithbc02@gmail.com>
2026-02-27 07:30:41 +0530

    Func Generic delegate

---
+++ FuncGenericDelegate/FuncGenericDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace FuncGenericDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<string> getName = () =>
+            {
+                return "Jannick";
+            };
+            var myName = getName();
+            Console.WriteLine(getName());
+
+            Func<int, int, int> sum = (x, y) =>
+            {
+                return x + y;
+            };
+            Console.WriteLine(sum(3,5));
+        }
+    }
+}
