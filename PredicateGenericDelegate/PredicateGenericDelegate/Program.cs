c4d75f99262aaaf29bbec94934b05f5637319e2a
mohithbc <mohithbc02@gmail.com>
2026-02-27 07:50:21 +0530

    Predicate generic delegate

---
+++ PredicateGenericDelegate/PredicateGenericDelegate/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PredicateGenericDelegate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = (x) =>
+            {
+                return x % 2 == 0;
+            };
+            // Console.WriteLine(isEven(5));
+            // We can use prdicate on a list of numbers to find even numbers
+            List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var evenInts = ints.FindAll(isEven);
+            foreach(var num in evenInts)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
