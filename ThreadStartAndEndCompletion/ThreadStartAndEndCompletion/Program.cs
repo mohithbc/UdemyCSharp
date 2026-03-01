a9a6c0017146f234f7623e302aef22c6d97c121d
MohithChandrashekhar <Mohith.Chandrashekhar@kalpitatechnologies.com>
2026-03-01 11:31:06 +0530

    Thread start and end completion

---
+++ ThreadStartAndEndCompletion/ThreadStartAndEndCompletion/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ThreadStartAndEndCompletion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            var thread = new Thread(() =>
+            {
+                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+                Thread.Sleep(1000);
+                taskCompletionSource.TrySetResult(true);
+                Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+            });
+            
+            thread.Start();
+            var test = taskCompletionSource.Task.Result;
+            Console.WriteLine("Task was done: {0}", test);
+        }
+    }
+}
