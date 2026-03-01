612fb3e9d36fe33258338e25c029069e1bb9143c
MohithChandrashekhar <Mohith.Chandrashekhar@kalpitatechnologies.com>
2026-03-01 11:41:27 +0530

    Thread pools

---
+++ ThreadPools/ThreadPools/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ThreadPools
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enumerable.Range(0, 1000).ToList().ForEach(f =>
+            {
+                ThreadPool.QueueUserWorkItem(o =>
+                {
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} started");
+
+                    Thread.Sleep(1000);
+
+                    Console.WriteLine($"Thread number: {Thread.CurrentThread.ManagedThreadId} ended");
+                });
+            });
+
+            Console.ReadLine(); // Prevent app from exiting immediately
+        }
+    }
+}
